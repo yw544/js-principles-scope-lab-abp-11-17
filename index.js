d8b7b31c6085b2bd96b95aeb91610965c9993668
York Wang <yw544@nyu.edu>
2017-11-28 08:08:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName()
+{
+return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer()
+{
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bobby";
+
+function changeLeastFavoriteCustomer()
+{
+  leastFavoriteCustomer = "bibby";
+}
+
+function attemptTwoFavoriteCustomers()
+{
+  let favoriteCustomer = "bb";
+  let favoriteCustomer = "cc";
+}
